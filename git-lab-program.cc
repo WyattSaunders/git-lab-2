30d8e69d6299ac12fce9019d1e2ae940b397db47
WyattSaunders <97543392+WyattSaunders@users.noreply.github.com>
2022-01-18 11:26:10 -0500

    Create git-lab-program.cc
    this says hi to git

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Wyatt Saunders>
+ *        Date: <tues jan 18 2022>
+ * Description: says hello to git
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+  cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
